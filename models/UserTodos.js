3dc036bdc90a2e43c039ce057146099db9c5d89e
violetacf <violeta@cique.es>
2022-11-23 11:48:45 +0100

    get and post req

--- models/UserTodos.js
+++ models/UserTodos.js
@@ -5,4 +5,15 @@ async function getAllUserToDos() {
   return result.rows;
 }
 
-module.exports = { getAllUserToDos };
+async function createToDo(toDo) {
+  const {user_todo_id, to_do_title} = toDo
+  const result = await query(`INSERT INTO user_todos(user_todo_id, to_do_title) VALUES ($1, $2)`, [user_todo_id, to_do_title]);
+
+  return result;
+}
+
+/*
+empty payload ^^^^
+*/
+
+module.exports = { getAllUserToDos, createToDo };
--- routes/userTodos.js
+++ routes/userTodos.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // this is where we put the models functions
-const { getAllUserTodos } = require("../models/UserTodos.js");
+const { getAllUserToDos, createToDo } = require("../models/UserTodos.js");
 
 router.get("/", async function (req, res) {
-  const response = await getAllUserTodos();
+  const response = await getAllUserToDos();
+  res.json({ success: true, payload: response });
+});
+
+router.post("/", async function (req, res) {
+  const toDoData = req.body
+  const response = await createToDo(toDoData);
   res.json({ success: true, payload: response });
 });
 
