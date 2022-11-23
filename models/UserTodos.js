8e89274b2052830d8e831960fa32d32eabe64daf
LouisWright <louis--wright@hotmail.co.uk>
2022-11-23 11:06:03 +0000

    backend requests set up

--- models/UserTodos.js
+++ models/UserTodos.js
@@ -6,14 +6,35 @@ async function getAllUserToDos() {
 }
 
 async function createToDo(toDo) {
-  const {user_todo_id, to_do_title} = toDo
-  const result = await query(`INSERT INTO user_todos(user_todo_id, to_do_title) VALUES ($1, $2)`, [user_todo_id, to_do_title]);
+  const { user_todo_id, to_do_title } = toDo;
+  const result = await query(
+    `INSERT INTO user_todos(user_todo_id, to_do_title) VALUES ($1, $2)`,
+    [user_todo_id, to_do_title]
+  );
 
-  return result;
+  return result.rows;
 }
 
 /*
 empty payload ^^^^
 */
 
-module.exports = { getAllUserToDos, createToDo };
+async function deleteToDo(id) {
+  const result = await query(
+    "DELETE FROM user_todos WHERE todo_id = $1 RETURNING *;",
+    [id]
+  );
+  return result.rows[0];
+}
+
+async function editToDo(id, updates) {
+  const { to_do_title } = updates;
+  const result = query(
+    "UPDATE user_todos SET to_do_title = $1 WHERE todo_id = $2 RETURNING *;",
+    [to_do_title, id]
+  );
+
+  return result.rowsd;
+}
+
+module.exports = { getAllUserToDos, createToDo, deleteToDo, editToDo };
--- routes/userTodos.js
+++ routes/userTodos.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 // this is where we put the models functions
-const { getAllUserToDos, createToDo } = require("../models/UserTodos.js");
+const {
+  getAllUserToDos,
+  createToDo,
+  deleteToDo,
+  editToDo,
+} = require("../models/UserTodos.js");
 
 router.get("/", async function (req, res) {
   const response = await getAllUserToDos();
@@ -10,9 +15,20 @@ router.get("/", async function (req, res) {
 });
 
 router.post("/", async function (req, res) {
-  const toDoData = req.body
+  const toDoData = req.body;
   const response = await createToDo(toDoData);
   res.json({ success: true, payload: response });
+});
+
+router.delete("/:id", async function (req, res) {
+  const result = await deleteToDo(req.params.id);
+  res.json({ success: true, payload: result });
+});
+
+router.patch("/:id", async function (req, res) {
+  const editData = req.body;
+  const result = await editToDo(req.params.id, editData);
+  res.json({ success: true, payload: result });
 });
 
 module.exports = router;
