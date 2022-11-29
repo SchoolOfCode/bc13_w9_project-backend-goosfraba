7e5386fb5cb1c499cd7d4671abec240d66e6eb91
IWLW <87724617+InstantW@users.noreply.github.com>
2022-11-29 15:11:13 +0000

    added documentation with JSdocs to our routes, models and database files

--- data/index.js
+++ data/index.js
@@ -1,3 +1,9 @@
+/**
+ * This is setting up our Non-blocking PostgreSQL client for Node.js.
+ * Setting up a new pool with the connection URL from our database .env variable contained in opur .env file
+ * Exporting our query function to be used in models.
+ */
+
 const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.POSTGRES_CONNECTION_URL,
